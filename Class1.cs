cccb4426545d22551041b4cde40589afc58c6d3a
Alexis Guerville <Alexis Guerville>
2017-01-23 17:30:59 +0100

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,87 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Database
+    {
+        string path;
+        public SQLite.Net.SQLiteConnection conn;
+        static Database instance;
+        public Database()
+        {
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "db.sqlite");
+            conn = new SQLite.Net.SQLiteConnection(new
+                SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+            conn.CreateTable<Task>();
+        }
+
+        static public Database getInstance()
+        {
+            if (instance == null)
+                instance = new Database();
+            return instance;
+        }
+    }
+
+    class TaskModel
+    {
+        static public void update(Task task)
+        {
+            Database.getInstance().conn.Update(task);
+        }
+        static public void add(Task task)
+        {
+            Database.getInstance().conn.Insert(task);
+        }
+
+        static public void removeById(int id)
+        {
+            Database.getInstance().conn.Execute("DELETE FROM Task WHERE id = ?", id);
+        }
+
+        static public List<Task> findAll()
+        {
+            List<Task> tasks = new List<Task>();
+            tasks = (from t in Database.getInstance().conn.Table<Task>()
+                        select t).ToList();
+            return tasks;
+        }
+
+        static public Task findById(int id)
+        {
+            Task task = new ToDoList.Task();
+            task = (from t in Database.getInstance().conn.Table<Task>()
+                    where t.id == id
+                    select t).FirstOrDefault();
+            return task;
+        }
+    }
+
+    class Task
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string content { get; set; }
+        public DateTime dueDate { get; set; }
+        public bool completed { get; set; }
+        
+        public Task()
+        {
+
+        }
+
+        public Task(string name, string content, DateTime date)
+        {
+            this.name = name;
+            this.content = content;
+            this.dueDate = date;
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace ToDoList
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = TaskModel.findAll();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Menu.IsPaneOpen = !Menu.IsPaneOpen;
+        }
+
+        private void TaskCheckbox_Check(object sender, RoutedEventArgs e)
+        {
+            int id = (int)(((CheckBox)sender).Tag);
+            Task task = TaskModel.findById(id);
+            task.completed = !task.completed;
+            TaskModel.update(task);
+        }
+
+        private void TaskDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int id = (int)(((Button)sender).Tag);
+            TaskModel.removeById(id);
+            DataContext = TaskModel.findAll();
+        }
+    }
+}
